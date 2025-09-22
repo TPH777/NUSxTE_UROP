5d15add263a5072e9c1599d866197b7d99d249b0
TPH777 <pinghuitan80@gmail.com>
2025-09-22 11:01:33 +0800

    Automate sd3.5 workflow to train and infer all classes at once

--- sd3.5/train.sh
+++ sd3.5/train.sh
@@ -14,24 +14,33 @@ source ~/sd3/bin/activate
 export ACCELERATE_CONFIG_FILE="/home/t/tph777/.cache/huggingface/accelerate/default_config.yaml"
 
 export VERSION="v2"
-export VALIDATION_PROMPT="NG - Single"
-export TRAIN_DIR="/home/t/tph777/training_set/train/$VALIDATION_PROMPT"
-export OUTPUT_DIR="/home/t/tph777/sd3.5/output/$VERSION/$VALIDATION_PROMPT"
+export TRAIN_ROOT="/home/t/tph777/training_set/train"
 
-accelerate launch --config_file $ACCELERATE_CONFIG_FILE ~/diffusers/examples/dreambooth/train_dreambooth_lora_sd3.py \
-  --pretrained_model_name_or_path="stabilityai/stable-diffusion-3-medium-diffusers"  \
-  --instance_data_dir="$TRAIN_DIR" \
-  --output_dir=$OUTPUT_DIR \
-  --mixed_precision="bf16" \
-  --instance_prompt="$VALIDATION_PROMPT" \
-  --resolution=256 \
-  --train_batch_size=1 \
-  --num_train_epochs=2 --checkpointing_steps=500 \
-  --max_train_steps=2000 \
-  --gradient_accumulation_steps=1 \
-  --learning_rate=4e-4 \
-  --lr_scheduler="constant" \
-  --lr_warmup_steps=0 \
-  --rank=16 \
-  --seed="0" \
-  --validation_prompt="$VALIDATION_PROMPT" \
+for SUBDIR in "$TRAIN_ROOT"/*/; do
+    # Remove trailing slash and get the subdirectory name
+    VALIDATION_PROMPT=$(basename "$SUBDIR")
+    TRAIN_DIR="$SUBDIR"
+    OUTPUT_DIR="/home/t/tph777/sd3.5/output/$VERSION/$VALIDATION_PROMPT"
+
+    echo "Training on $TRAIN_DIR with prompt '$VALIDATION_PROMPT'"
+
+    accelerate launch --config_file $ACCELERATE_CONFIG_FILE ~/diffusers/examples/dreambooth/train_dreambooth_lora_sd3.py \
+      --pretrained_model_name_or_path="stabilityai/stable-diffusion-3-medium-diffusers"  \
+      --instance_data_dir="$TRAIN_DIR" \
+      --output_dir="$OUTPUT_DIR" \
+      --mixed_precision="bf16" \
+      --instance_prompt="$VALIDATION_PROMPT" \
+      --resolution=256 \
+      --train_batch_size=1 \
+      --num_train_epochs=2 --checkpointing_steps=500 \
+      --max_train_steps=2000 \
+      --gradient_accumulation_steps=1 \
+      --learning_rate=4e-4 \
+      --lr_scheduler="constant" \
+      --lr_warmup_steps=0 \
+      --rank=16 \
+      --seed="0" \
+      --validation_prompt="$VALIDATION_PROMPT"
+done
+
+deactivate
